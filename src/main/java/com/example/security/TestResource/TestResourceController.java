a76c40b52a13cd82313c818953481ae1d9ba0532
ygetachew <getachewy307@gmail.com>
2025-07-03 16:13:38 +0300

    initial

--- src/main/java/com/example/security/TestResource/TestResourceController.java
+++ src/main/java/com/example/security/TestResource/TestResourceController.java
@@ -1,18 +1,30 @@
 package com.example.security.TestResource;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.Document;
+
+import java.security.cert.Certificate;
 
 @RestController
 @RequestMapping("/security")
+@RequiredArgsConstructor
 public class TestResourceController {
     @GetMapping("")
 
     public String test(){
     return "passed";
 }
+
+
+
 
 
 }
